a8357c99846a2b9e7ef99699fcdffd4a23b0f36c
Amit Narang <amitnarang.pers@gmail.com>
2020-05-29 15:27:26 -0400

    Done w/ V2

--- src/problem/tester.py
+++ src/problem/tester.py
@@ -4,7 +4,8 @@ parser = Standard()
 
 parser.setFEPolynomialDegree(4)
 parser.setSpatialMax([1,2,3,4,5])
+parser.setSpatialDimension(2)
 parser.snapshot()
 parser.setTransportModel(True)
-
+parser.setOutputFilenameBase("howdy")
 parser.saveAs("temp/inputSamples")
